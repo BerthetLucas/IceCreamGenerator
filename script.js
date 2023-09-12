cca3b4313372f28211679809b9c9132195a44392
Lucas Berthet <lu.berthet@gmail.com>
2023-09-12 13:13:58 +0200

    fixed choices issues when different scoop number was chosen

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ oneScoopContainer = document.querySelector(".oneScoop-container");
 twoScoopContainer = document.querySelector(".twoScoop-container");
 threeScoopContainer = document.querySelector(".threeScoop-container");
 
-flavourOneImg = document.querySelector('.flavourOneImg'); 
-flavourTwoImg = document.querySelector('.flavourTwoImg');
-flavourThreeImg = document.querySelector('.flavourThreeImg');
-
+flavourOneImg = document.querySelector(".flavourOneImg");
+flavourTwoImg = document.querySelector(".flavourTwoImg");
+flavourThreeImg = document.querySelector(".flavourThreeImg");
 
 oneScoopBtn = document.querySelector(".oneScoop");
 twoScoopBtn = document.querySelector(".twoScoop");
 threeScoopBtn = document.querySelector(".threeScoop");
+
+function reset() {
+  oneScoopContainer.style.display = "none";
+  twoScoopContainer.style.display = "none";
+  threeScoopContainer.style.display = "none";
+  flavourOneImg.style.display = "none";
+  flavourTwoImg.style.display = "none";
+  flavourThreeImg.style.display = "none";
+}
 
 document.addEventListener("DOMContentLoaded", function () {
   fetch("flavour.json")
@@ -20,28 +28,36 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log(flavourdata);
 
       oneScoopBtn.addEventListener("click", function () {
+        reset();
+
         let randomIdex = Math.floor(Math.random() * 11);
 
         oneScoopContainer.innerHTML = flavourdata[randomIdex].viande;
         oneScoopContainer.style.display = "block";
+        flavourOneImg.style.display = "block";
         flavourOneImg.src = `${flavourdata[randomIdex].img}`;
       });
 
       twoScoopBtn.addEventListener("click", function () {
+        reset();
         let randomIdex = Math.floor(Math.random() * 11);
         let randomIdexA = Math.floor(Math.random() * 11);
 
         oneScoopContainer.innerHTML = flavourdata[randomIdex].viande;
         oneScoopContainer.style.display = "block";
+        flavourOneImg.style.display = "block";
+
         flavourOneImg.src = `${flavourdata[randomIdex].img}`;
 
         twoScoopContainer.innerHTML = flavourdata[randomIdexA].viande;
         twoScoopContainer.style.display = "block";
+        flavourTwoImg.style.display = "block";
         flavourTwoImg.src = `${flavourdata[randomIdexA].img}`;
-
       });
 
       threeScoopBtn.addEventListener("click", function () {
+
+        reset();
 
         let randomIdex = Math.floor(Math.random() * 11);
         let randomIdexA = Math.floor(Math.random() * 11);
@@ -50,17 +66,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
         oneScoopContainer.innerHTML = flavourdata[randomIdex].viande;
         oneScoopContainer.style.display = "block";
+        flavourOneImg.style.display = "block";
         flavourOneImg.src = `${flavourdata[randomIdex].img}`;
 
         twoScoopContainer.innerHTML = flavourdata[randomIdexA].viande;
         twoScoopContainer.style.display = "block";
+        flavourTwoImg.style.display = "block";
         flavourTwoImg.src = `${flavourdata[randomIdexA].img}`;
 
         threeScoopContainer.innerHTML = flavourdata[randomIdexB].viande;
         threeScoopContainer.style.display = "block";
-        flavourThreeImg.src = `${flavourdata[randomIdexB].img}`
-
+        flavourThreeImg.style.display = "block";
+        flavourThreeImg.src = `${flavourdata[randomIdexB].img}`;
       });
-
     });
 });
